aa5fdf57318233020cc8672e4b1ca70471122b04
gcygan2 <prezentacje@gcygan.webd.pl>
2026-03-23 13:37:30 +0100

    Update rtc.ino

--- rtc/rtc.ino
+++ rtc/rtc.ino
@@ -1,40 +1,25 @@
 #include <Wire.h>
-
-#define PCF8583_ADDR 0x50
-#define DS3231_ADDR  0x68
-
+#define RTC_ADDR 0x68/*pcf8583 0x50, ds3231 0x68*/
+#define RTC_SEC 0x00/*pcf8583 0x02, ds3231 0x00*/
 void setup() {
   Serial.begin(115200);
   Wire.begin(21, 22); // SDA, SCL
   delay(1000);
 }
-
-// Funkcja konwertuj¹ca BCD na decimal
 byte bcdToDec(byte val) {
   return (val / 16 * 10) + (val % 16);
 }
-
-// Funkcja odczytu sekund dla RTC
 byte readSeconds(uint8_t rtc_addr, uint8_t reg_sec) {
   Wire.beginTransmission(rtc_addr);
   Wire.write(reg_sec);
   Wire.endTransmission();
   Wire.requestFrom(rtc_addr, (uint8_t)1);
-
   byte sec = 0;
   if (Wire.available()) sec = Wire.read() & 0x7F; // maska bit7
   return bcdToDec(sec);
 }
-
 void loop() {
-  // Odczyt sekund
-  byte secPCF  = readSeconds(PCF8583_ADDR, 0x02);
-  byte secDS   = readSeconds(DS3231_ADDR, 0x00);
-
-  Serial.print("PCF8583 sekundy: ");
-  Serial.print(secPCF);
-  Serial.print(" | DS3231 sekundy: ");
-  Serial.println(secDS);
-
-  delay(1000);
+  byte sec  = readSeconds(RTC_ADDR, RTC_SEC);  // Odczyt sekund
+  Serial.println(sec);
+  delay(500);
 }
