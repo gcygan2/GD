60fdd7508a02909a65f57d75c447fd6d0a787594
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2026-04-03 10:46:43 +0200

    Update expander.ino

--- expander/expander.ino
+++ expander/expander.ino
@@ -2,20 +2,22 @@
 #include <HTTPClient.h>
 #include <Wire.h>
 #define PCF8575_ADDR 0x20
-const char* ssid = "TWOJE_SSID";
-const char* password = "TWOJE_HASLO";
+const char* ssid = "SSID";
+const char* password = "PASS";
 const char* serverUrl = "http://gcygan.webd.pl/ob/ekran.php";
+
 void writePCF8575(uint16_t value) {
   Wire.beginTransmission(PCF8575_ADDR);
   Wire.write(value & 0xFF);        // mlodszy bajt
   Wire.write((value >> 8) & 0xFF); // starszy bajt  
   Wire.endTransmission();
 }
+
 void setup() {
   Serial.begin(115200);
-  Wire.begin(); // I2C start
+  Wire.begin();
   WiFi.begin(ssid, password);
-  Serial.print("£¹czenie z WiFi");
+  Serial.print("Lczenie z WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -29,10 +31,14 @@ void loop() {
     http.begin(serverUrl);
     int httpCode = http.GET();
     
-    if (httpCode > 0) {
-      String payload = http.getString();
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();    
+      Serial.print("Odebrano: ");
+      Serial.println(payload);
       uint16_t value = (uint16_t) payload.toInt();
-      writePCF8575(value);
+      Serial.print("Wartosc: ");
+      Serial.println(value);
+      writePCF8575(~value);
     } else {
       Serial.print("Blad HTTP: ");
       Serial.println(httpCode);
